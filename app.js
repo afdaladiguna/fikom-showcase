02900135fd3bda32b5e7c7b504f4ca4cea5caf36
afdaladiguna <m.afdal.adiguna@gmail.com>
2023-11-03 22:52:18 +0800

    add JOI for validations

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+const { campgroundSchema } = require('./schemas.js');
 const ExpressError = require('./utils/ExpressError');
 const catchAsync = require('./utils/catchAsync');
 const Campground = require('./models/campground');
@@ -24,6 +25,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true })); // parse the req.body
 app.use(methodOverride('_method'));
 
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
 app.get('/', (req, res) => {
   res.send('HOME!');
 });
@@ -42,11 +53,14 @@ app.get('/campgrounds/new', (req, res) => {
 
 app.post(
   '/campgrounds',
+  validateCampground,
   catchAsync(async (req, res, next) => {
-    if (!req.body.campground) {
-      throw new ExpressError('Invalid Campground Data', 400);
-    }
+    // if (!req.body.campground) {
+    //   throw new ExpressError('Invalid Campground Data', 400);
+    // }
+
     const campground = new Campground(req.body.campground);
+    // console.log(campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
   })
@@ -70,6 +84,7 @@ app.get(
 
 app.put(
   '/campgrounds/:id',
+  validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {
