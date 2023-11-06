a8a18028f0239cd61354896a1625c2b00353c26d
afdaladiguna <m.afdal.adiguna@gmail.com>
2023-11-06 16:18:58 +0800

    adding reviews

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const { Schema } = mongoose;
 
 const CampgroundSchema = new Schema({
@@ -7,6 +8,24 @@ const CampgroundSchema = new Schema({
   price: Number,
   description: String,
   location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review',
+    },
+  ],
+});
+
+CampgroundSchema.post('findOneAndDelete', async (doc) => {
+  if (doc) {
+    // delete all reviews where their ID field is in
+    // the 'doc' or in the deleted campground
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
