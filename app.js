bf6ac9c90ac5bf009af67f7655555b30254218d5
afdaladiguna <m.afdal.adiguna@gmail.com>
2023-11-21 12:47:03 +0800

    styles clean up

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
-  res.redirect('/campgrounds');
+  res.render('home');
 });
 
 app.all('*', (req, res, next) => {
