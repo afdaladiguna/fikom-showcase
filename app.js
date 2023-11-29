d003341afecb78e065671c4ba5ffd520c46e3357
afdaladiguna <m.afdal.adiguna@gmail.com>
2023-11-29 09:58:35 +0800

    fix deploying

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const User = require('./models/user');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl);
 
